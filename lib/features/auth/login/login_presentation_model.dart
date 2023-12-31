e43ebf87e95f238ce1a2424067e51ddd99691623
abdalkader khe <65679396+abdalkaderKhe@users.noreply.github.com>
2023-12-31 14:30:51 +0300

    Update login_presentation_model.dart

--- lib/features/auth/login/login_presentation_model.dart
+++ lib/features/auth/login/login_presentation_model.dart
@@ -1,55 +1,52 @@
-
 import 'package:flutter_demo/features/auth/login/login_initial_params.dart';
-
-
 
 /// Model used by presenter, contains fields that are relevant to presenters and implements ViewModel to expose data to view (page)
 class LoginPresentationModel implements LoginViewModel {
   /// Creates the initial state
   LoginPresentationModel.initial(
-    // ignore: avoid_unused_constructor_parameters
-    LoginInitialParams initialParams,
-  ) : isLoginEnabled = false,
-      isLoading = false;
-
-  @override
-  final bool isLoginEnabled;
-
-  @override
-  final bool isLoading;
-
-
-
+      // ignore: avoid_unused_constructor_parameters
+      LoginInitialParams initialParams,
+      );
 
   /// Used for the copyWith method
-  LoginPresentationModel._({
-    required this.isLoginEnabled,
-    required this.isLoading,
-  });
+  LoginPresentationModel._(String? userName, String? password,bool? isLoginEnabled,bool? isLoadingEnabled,)
+      : _userName = userName,
+        _password = password,
+        _isLoginEnabled = isLoginEnabled,
+        _isLoadingEnabled = isLoadingEnabled;
 
+  String? _userName;
+  String? _password;
+  bool? _isLoginEnabled;
+  bool? _isLoadingEnabled;
 
-  LoginPresentationModel copyWith({
-    bool? isLoginEnabled,
-    bool? isLoading,
-    bool? loginIsFails,
-    bool? loginIsSuccessfully,
-  }) {
+  @override
+  String get password => _password ?? '';
+
+  @override
+  String get userName => _userName ?? '';
+
+  @override
+  bool get isLoadingEnabled => _isLoadingEnabled?? false;
+
+  @override
+  bool get isLoginEnabled => _isLoginEnabled ?? false;
+
+  LoginPresentationModel copyWith({String? userName, String? password,bool? isLoginEnabled,bool? isLoadingEnabled,}) {
     return LoginPresentationModel._(
-      isLoginEnabled: isLoginEnabled ?? this.isLoginEnabled,
-      isLoading: isLoading ?? this.isLoading,
+      userName ?? this.userName,
+      password ?? this.password,
+      isLoginEnabled ?? this.isLoginEnabled,
+      isLoadingEnabled ?? this.isLoadingEnabled,
     );
   }
-
-
-
-
-
 }
 
 /// Interface to expose fields used by the view (page).
 abstract class LoginViewModel {
+  String get userName;
+  String get password;
   bool get isLoginEnabled;
-  bool get isLoading;
+  bool get isLoadingEnabled;
 }
-
 
